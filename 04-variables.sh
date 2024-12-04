9ea223642520afe4270cb30eb8407ac2b7206e4e
meenakshi <meena@gmail.com>
2024-12-04 19:26:40 +0530

    variable values changed

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$2
+person2=raheem
+echo "$person1:: Heloo $person2, good morning"
+echo "$person2:: Hi $person1, very good morning"
+echo "$person1:: how are you $person2?"
+echo "$person2:: i am goo $person1, How are you"
