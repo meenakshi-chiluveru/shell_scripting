04429ee687624858556d152b45825cebd2ed19bf
meenakshi <meena@gmail.com>
2024-12-04 19:48:38 +0530

    variable values changed

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-USERNAME=$1
-PASSWORD=$2
+#USERNAME=$1
+#PASSWORD=$2
 
-echo "username is $USERNAME, password is $PASSWORD"
+# echo "username is $USERNAME, password is $PASSWORD"
+
+echo "please enter your username"
+read USERNAME #the value entered above will be automatically attatched to USERNAME variable
+
+echo "username is: $USERNAME"
