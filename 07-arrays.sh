5aaa1a311e207bd18fd84f887bbeb80e20988fdb
meenakshi <meena@gmail.com>
2024-12-04 20:16:13 +0530

    variable values changed

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,4 +4,4 @@ fruits=("apple" "banana" "mango")
 echo "first value: ${fruits[0]}"
 echo "second value: ${fruits[1]}"
 echo "third value: ${fruits[2]}"
-echo "all fruits:${fruits[@]}"
+echo "all fruits: ${fruits[@]}"
