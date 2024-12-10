aad1acf9a966e60bcd1219a7005a51a6c2e95685
meenakshi <meena@gmail.com>
2024-12-10 19:23:58 +0530

    install packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -4,6 +4,20 @@ ID=$(ID -U)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m" # normal color
+Y="\e[33m"
+timestamp=$(date +%F-%F-%M-%S)
+logfile="/tmp/$0-$timestamp.log"
+
+echo "script started executing at $timestamp" &>> $logfile
+
+validate(){
+  if [ $! -ne 0 ]
+  then
+    echo -e "$2 .. $R failed $N"
+  else
+    echo -e "$2 .. $G success $N"
+  fi
+}
 
 if [ $ID -ne 0 ]
 then
@@ -13,4 +27,17 @@ else
   echo "you are root user"
 fi # fi means reverse of if, indicating condition end
 
-echo "All arguments passed:$@"
+
+ #echo "All arguments passed: $@"
+
+ for package in $@
+ do
+   yum list installed $package  &>> $logfile
+   if [ $? -ne 0 ] # if not installed
+   then
+     yum install $package -y &>> $logfile
+     validate $? "installation of $package"
+   else
+     echo -e "$package is already installed ..$Y skipping $N"
+    fi
+ done
